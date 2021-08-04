1eea7a5f7164e0aa44f18d355389d402ec701f98
aaronyu6 <eatless$3Q>
2021-08-04 17:21:11 +0800

    https work

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as http from 'http';
+import * as https from 'https';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const certPath = join(__dirname, '..', '/cert');
+  const httpsOptions = {
+    key: fs.readFileSync(certPath + '/server.key'),
+    cert: fs.readFileSync(certPath + '/server.crt'),
+  };
+
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+    { cors: true }
+  );
+  //await app.listen(3000);
+  await app.init();
+
+  http.createServer(server).listen(3000);
+  https.createServer(httpsOptions, server).listen(1443);
 }
 bootstrap();
